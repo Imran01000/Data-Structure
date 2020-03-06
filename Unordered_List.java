0d5ba2edafba048b25e85cfe738e35cd4cb82593
Imran01000 <imshaik01000@gmail.com>
2020-03-06 12:54:28 +0530

    ADDED:Added feature to insert the string to th existing file.

--- Unordered_List.java
+++ Unordered_List.java
@@ -1,63 +1,76 @@
 import java.io.*;
+
 class Node {
 	String words;
 	Node next;
 }
-
+// Main class
 public class Unordered_List{
+	String line="";
+	Node head;
 	
-//	Node head;
-//	private void insert(String words)
-//	{
-//		Node node = new Node();
-//		node.words=words;
-//		node.next=null;
-//		if(head == null)
-//		{
-//			head = node;
-//
-//		}
-//		else
-//		{
-//			Node n=head;
-//			while(n.next!=null)
-//			{
-//				n=n.next;
-//			}
-//			n.next=node;
-//		}
-//	}
-//	private void show()
-//	{
-//		Node p=head;
-//		while(p.next!=null)
-//		{
-//			System.out.println(p.words);
-//			p=p.next;
-//		}
-//	}
+	//To insert the string in the list.
+	private void insert(String words)
+	{
+		Node node = new Node();
+		node.words = words;
+		node.next = null;
+		if(head == null)
+		{
+			head = node;
+
+		}
+		else
+		{
+			Node traverse = head;
+			while(traverse.next != null)
+			{
+				traverse = traverse.next;
+			}
+			traverse.next = node;
+		}
+	}
+	
+	//To Display the list.
+	private void show()
+	{
+		Node traverse = head;
+		while(traverse.next != null)
+		{
+			System.out.println(traverse.words);
+			traverse = traverse.next;
+		}
+	}
 	public static void main(String[] args) throws IOException
 	{	
 		Unordered_List list = new Unordered_List();
-		File demoFile = new File("demoFile.txt");
-		demoFile.createNewFile();
 		FileReader file = new FileReader("demoFile.txt");
 		BufferedReader br = new BufferedReader(file);
 		String line = br.readLine();
-		String[] array = new String[100];
-		int i=0;
-		while(i==array.length)
+		String[] l = line.split(" ");
+		try {
+
+			while(line != null)
+			{
+				for (String s : l) 
+				{		
+					list.insert(s);
+				}
+				line = br.readLine();
+				l = line.split(" ");
+
+			}
+
+		}catch(NullPointerException e)
 		{
-			array[i]=line;
+			System.out.println();
 		}
-		array = line.split(" ");
-		
-		while(line!=null || i==array.length)
-		{
-			System.out.println(array[i]);
-			line = br.readLine();
-			i++;
-		}
+
+		System.out.println("linked List value");
+		list.insert("Hey");
+		list.insert("I am new here");
+		list.insert("How are you all?");
+		list.show();
 		br.close();
 	}
 }
